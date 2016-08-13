a6904cb8bb8d0e42e4aaca09f5342387dc8f6725
edvin <es@syse.no>
2016-08-13 17:13:21 +0200

    Updated example to use DataSourceProvider

--- javaee-example/src/main/kotlin/no/tornado/kdbc/examples/javaee/KDBCApplication.kt
+++ javaee-example/src/main/kotlin/no/tornado/kdbc/examples/javaee/KDBCApplication.kt
@@ -1,6 +1,6 @@
 package no.tornado.kdbc.examples.javaee
 
-
+import kdbc.KDBC
 import kdbc.execute
 import javax.annotation.PostConstruct
 import javax.annotation.Resource
@@ -9,16 +9,25 @@ import javax.ejb.Startup
 import javax.sql.DataSource
 
 /**
- * This class is only used to bootstrap some data and has nothing to do with
- * how you would configure KDBC in a JavaEE environment.
+ * This class configures a default data source for Queries.
+ *
+ * Not that the DataSourceProvider can return different data sources
+ * based on the query provided to it.
+ *
+ * Alternatively you can supply a datasource on a per-query basis via the Query.db() function.
+ *
+ * This class also provides some default data to work with for the demo.
+ *
  */
 @Startup
 @Singleton
-open class CreateExampleData {
+open class KDBCApplication {
     @Resource lateinit private var dataSource: DataSource
 
     @PostConstruct
     open fun init() {
+        KDBC.DataSourceProvider = { dataSource }
+
         with(dataSource) {
             execute("DROP TABLE IF EXISTS customer")
             execute("CREATE TABLE customer (id integer not null primary key auto_increment, name text)")
--- javaee-example/src/main/kotlin/no/tornado/kdbc/examples/javaee/services/CustomerService.kt
+++ javaee-example/src/main/kotlin/no/tornado/kdbc/examples/javaee/services/CustomerService.kt
@@ -1,24 +1,14 @@
 package no.tornado.kdbc.examples.javaee.services
 
-import kdbc.db
 import no.tornado.kdbc.examples.javaee.models.Customer
 import no.tornado.kdbc.examples.javaee.models.SelectCustomer
 import no.tornado.kdbc.examples.javaee.models.UpdateCustomer
-import javax.annotation.Resource
 import javax.ejb.Stateless
-import javax.sql.DataSource
 
 @Stateless
 open class CustomerService {
-    @Resource lateinit private var ds: DataSource
-
-    // Convenience function to instantiate the SelectCustomer query and assign a database connection to it.
-    // This could probably be improved in a declarative way, suggestions are welcome.
-    // Granted, it is very explicit and easy to reason about so it feels like "The Kotlin Way(TM)"
-    private fun select() = SelectCustomer().db(ds)
-
-    open fun list() = select().list()
-    open fun byId(id: Int) = select().byId(id)
-    open fun search(term: String) = select().search(term)
-    open fun update(customer: Customer) = UpdateCustomer(customer).execute(ds)
+    open fun list() = SelectCustomer().list()
+    open fun byId(id: Int) = SelectCustomer().byId(id)
+    open fun search(term: String) = SelectCustomer().search(term)
+    open fun update(customer: Customer) = UpdateCustomer(customer).execute()
 }
